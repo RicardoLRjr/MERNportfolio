454c95810fdd5ae3d102295346db1766aecaf999
Ricardo Russ <ricardoruss@live.com>
2020-05-22 01:42:40 -0400

    path.join fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
-
+express.static(path.join(__dirname, '../client/build'))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
