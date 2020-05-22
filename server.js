07dfc300ac9fb274eba46bb71a5d0f6d7205e5ff
Ricardo Russ <ricardoruss@live.com>
2020-05-22 01:17:46 -0400

    what in the world

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
