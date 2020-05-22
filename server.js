fb9fc184a91dbe61bab5275e5d2119d4a592f289
Ricardo Russ <ricardoruss@live.com>
2020-05-22 01:29:55 -0400

    testing new build method

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 require("dotenv").config();
 const express = require("express");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +14,12 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
 });
